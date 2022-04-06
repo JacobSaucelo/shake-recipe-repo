c918ef5b51bb8b0d2e3ab8fb9b8587b9ba514929
Jacob Saucelo <jacobsaucelo32@gmail.com>
2022-04-06 10:26:13 +0800

    updated cors

--- api/server.js
+++ api/server.js
@@ -13,7 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(cors());
+// app.use(
+//   cors({
+//     origin: "*",
+//   })
+// );
 app.use(
   cors({
     origin: "http://localhost:3000",
